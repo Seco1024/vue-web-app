2b5cb873eff752143d783f4970c190de57fce216
Deyen Chiu <deyenqiu@gmail.com>
2022-11-25 02:40:58 +0800

    add order record's tags filter

--- src/views/System/Record/Product/components/MainTable.vue
+++ src/views/System/Record/Product/components/MainTable.vue
@@ -2,40 +2,51 @@
     <el-table :data="tableData" style="width: 100%" max-height="75vh">
         <el-table-column type="expand">
             <template #default="scope">
-              <div class="note">
-                <p m="t-0 b-2">備註：{{ scope.row.note }} </p>
-              </div>
-              <div class="expand">
-                <el-table :data="scope.row.productList" stripe show-summary > 
-                  <el-table-column prop="productName" label="名稱" width="180" />
-                  <el-table-column prop="amount" label="數量" width="80"/>
-                  <el-table-column prop="price" label="價錢" width="80"/>
-                </el-table>
-              </div>
+                <div class="note">
+                    <p m="t-0 b-2">備註：{{ scope.row.note }}</p>
+                </div>
+                <div class="expand">
+                    <el-table :data="scope.row.productList" stripe show-summary>
+                        <el-table-column prop="productName" label="名稱" width="180" />
+                        <el-table-column prop="amount" label="數量" width="80" />
+                        <el-table-column prop="price" label="價錢" width="80" />
+                    </el-table>
+                </div>
             </template>
         </el-table-column>
         <el-table-column prop="uid" label="訂單編號" show-overflow-tooltip />
         <el-table-column prop="timestamp" label="建立時間" show-overflow-tooltip />
         <el-table-column prop="totalPrice" label="總價" width="80" />
-        <el-table-column label="標籤" width="200">
-          <template #default="scope">
-            <el-tag v-for="item in scope.row.tagList" class="tag" effect="plain" closable>{{item}}</el-tag>
-          </template>
+        <el-table-column
+            label="標籤"
+            width="200"
+            :filters="[
+                { text: '製作中', value: '製作中' },
+                { text: '瑕疵品', value: '瑕疵品' },
+                { text: '外送', value: '外送' },
+            ]"
+            :filter-method="filterTag"
+            filter-placement="bottom-end"
+        >
+            <template #default="scope">
+                <el-tag v-for="item in scope.row.tagList" class="tag" effect="plain" closable @close="handleCloseTag(item, scope.row.uid)">
+                    {{ item }}
+                </el-tag>
+            </template>
         </el-table-column>
         <el-table-column label="" width="70">
-          <template #default="scope">
-            <el-button type="danger" :icon="Delete" circle @click="deleteOrder(scope.row.uid)" />
-          </template>
+            <template #default="scope">
+                <el-button type="danger" :icon="Delete" circle @click="handleDelete(scope.row.uid)" />
+            </template>
         </el-table-column>
     </el-table>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive} from 'vue'
-import { Delete } from '@element-plus/icons-vue'
-import type { TableColumnCtx } from 'element-plus/es/components/table/src/table-column/defaults'
+import { ref, reactive } from "vue"
+import { Delete } from "@element-plus/icons-vue"
 
-const tag = ref('')
+const tag = ref("")
 const form = reactive({
     uid: "",
     timestamp: "",
@@ -43,138 +54,157 @@ const form = reactive({
     note: "",
     productList: [
         {
-            productName:"",
+            productName: "",
             productId: "",
             amount: "",
-            price: ""
+            price: "",
         },
     ],
     tagList: [
         {
-            label: "",
-            value: ""
-        }
-    ]
+            value: "",
+        },
+    ],
 })
 
-const deleteOrder = (uid) => {
-    console.log(uid)
+interface Order {
+    uid: string
+    timestamp: string
+    totalPrice: Number
+    note: string
+    productList: {
+        productName: string
+        productId: string
+        amount: Number
+        price: Number
+    }[]
+    tagList: string[]
 }
 
-// const filterTag = (value: string, row: User) => {
-//   return row.tag === value
-// }
+const filterTag = (value: string, row: Order) => {
+    console.log(row.tagList)
+    console.log(row.tagList.some((tag) => tag.valueOf() === value))
+    return row.tagList.some((tag) => tag.valueOf() === value)
+}
 
 const tableData = [
     {
-        uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
+        uid: "CHpImey2IsMHIzZTJRBUHKopSWU1",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
         tagList: ["製作中"],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 60
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 60,
+            },
         ],
     },
     {
         uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
-        tagList: ["瑕疵品", "製作中","外送"],
+        tagList: ["瑕疵品", "製作中", "外送"],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 500
-          },
-          {
-            productName: "蘋果牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 4,
-            price: 500
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 500,
+            },
+            {
+                productName: "蘋果牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 4,
+                price: 500,
+            },
         ],
     },
     {
-        uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
+        uid: "CHpImey2IsMHIzZTJRBUHKopSWU3",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
         tagList: ["瑕疵品"],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 60
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 60,
+            },
         ],
     },
     {
-        uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
+        uid: "CHpImey2IsMHIzZTJRBUHKopSWU4",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
         tagList: ["瑕疵品", "外送"],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 60
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 60,
+            },
         ],
     },
     {
-        uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
+        uid: "CHpImey2IsMHIzZTJRBUHKopSWU5",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
         tagList: [],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 60
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 60,
+            },
         ],
     },
     {
-        uid: "CHpImey2IsMHIzZTJRBUHKopSWU2",
+        uid: "CHpImey2IsMHIzZTJRBUHKopSWU6",
         timestamp: "2022年10月29日 凌晨 12:19:42 [UTC+8]",
         totalPrice: 1000,
         tagList: ["外送", "製作中"],
         note: "這是備註",
         productList: [
-          {
-            productName: "可可牛奶",
-            productId: "MPYIaoHT8Iz704piU30S",
-            amount: 2,
-            price: 60
-          }
+            {
+                productName: "可可牛奶",
+                productId: "MPYIaoHT8Iz704piU30S",
+                amount: 2,
+                price: 60,
+            },
         ],
-    }
+    },
 ]
 
+const handleDelete = (uid) => {
+    console.log(uid)
+}
+
+const handleCloseTag = (tag, uid) => {
+    console.log(tag, uid)
+}
 </script>
 
 <style>
 .note {
-    margin:15px
-    
+    margin: 15px;
 }
-.expand{
-    margin-left:15px
+.expand {
+    margin-left: 15px;
+    margin-bottom: 10px;
 }
-.tag{
+.tag {
     margin-top: 3px;
+    margin-right: 3px;
 }
 </style>
