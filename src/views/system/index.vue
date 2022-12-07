bebca5464e0f91dcc66291d7ce911e2ec34156d6
YJack0000 <jackleagends@gmail.com>
2022-12-08 03:54:58 +0800

    fix system file problem

---
+++ src/views/system/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import SystemLayout from "@/layouts/SystemLayout.vue"
+import SystemAside from "@/components/system/SystemAside.vue"
+import SystemHeader from "@/components/system/SystemHeader.vue"
+</script>
+
+<template>
+    <SystemLayout>
+        <template #aside>
+            <SystemAside />
+        </template>
+        <template #header>
+            <SystemHeader />
+        </template>
+        <template #main>
+            <router-view />
+        </template>
+    </SystemLayout>
+</template>
+
+<style scoped></style>
