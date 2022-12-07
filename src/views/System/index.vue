cb82d63241b9cd4fcbc6c56077bb6223ffa9c853
YJack0000 <jackleagends@gmail.com>
2022-12-07 16:24:54 +0800

    add i18n

--- src/views/System/index.vue
+++ src/views/System/index.vue
@@ -2,20 +2,29 @@
 import SystemLayout from "@/layouts/SystemLayout.vue"
 import SystemAside from "@/components/system/SystemAside.vue"
 import SystemHeader from "@/components/system/SystemHeader.vue"
+
+import { ref } from "vue"
+import { ElConfigProvider } from "element-plus"
+
+import zhTw from "element-plus/dist/locale/zh-tw.mjs"
+
+const locale = ref(zhTw)
 </script>
 
 <template>
-    <SystemLayout>
-        <template #aside>
-            <SystemAside />
-        </template>
-        <template #header>
-            <SystemHeader />
-        </template>
-        <template #main>
-            <router-view />
-        </template>
-    </SystemLayout>
+    <el-config-provider :locale="locale">
+        <SystemLayout>
+            <template #aside>
+                <SystemAside />
+            </template>
+            <template #header>
+                <SystemHeader />
+            </template>
+            <template #main>
+                <router-view />
+            </template>
+        </SystemLayout>
+    </el-config-provider>
 </template>
 
 <style scoped></style>
